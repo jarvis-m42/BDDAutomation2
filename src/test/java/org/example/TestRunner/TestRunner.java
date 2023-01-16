ffb1af64c1983221cca7e59f4b4d98396b3478a5
bhosalr <rohit.bhosale@autodesk.com>
2023-01-16 16:11:48 +0530

    init commit

---
+++ src/test/java/org/example/TestRunner/TestRunner.java
@@ -0,0 +1,46 @@
+package org.example.TestRunner;
+
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
+import io.cucumber.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+@CucumberOptions(
+        features = "src/test/resources/features/CarneraHomepage.feature",
+        glue = {"org.example.stepdef"},
+        tags = "@UATTesting",plugin = {"pretty",
+        "html:target/site/cucumber-pretty.html",
+        "json:target/cucumber.json"},
+        monochrome = true)
+public class TestRunner {
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass()throws Exception{
+        testNGCucumberRunner=new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(dataProvider = "scenarios")
+    public void feature(PickleWrapper pickle, FeatureWrapper cucumberFeature){
+        testNGCucumberRunner.runScenario(pickle.getPickle());
+    }
+
+    @DataProvider
+    public Object[][] scenarios() {
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() throws Exception{
+        testNGCucumberRunner.finish();
+    }
+
+
+
+
+
+}
---
+++ src/test/java/org/example/Utility/PropertyFileReader.java
@@ -0,0 +1,18 @@
+package org.example.Utility;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertyFileReader {
+
+    public Properties getProperty(){
+        Properties properties=new Properties();
+        try{
+            properties.load(new FileInputStream("src/resources/browser-config.properties"));
+        }catch (Exception e){
+            System.out.println("Exception: "+e);
+        }
+        return properties;
+    }
+
+}
